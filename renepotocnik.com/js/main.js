6ba798cfa8697df81e5ab4305bac5a4008313e64
Rene Potocnik <rene.potocnik1@gmail.com>
2023-03-17 07:00:21 +0100

    Bring back text stagger

---
+++ renepotocnik.com/js/main.js
@@ -0,0 +1,54 @@
+// Animate elements with class "hidden" when they become visible in the viewport.
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('showing');
+        } else {
+            entry.target.classList.remove('showing');
+        }
+    });
+});
+
+
+// Title Text Stagger effect
+
+function splitLetters(className) {
+    const parent = document.querySelector(`.${className}`);
+    const txt = parent.innerText;
+    parent.innerText = "";
+
+    // Split the sentence into letters and create a `span` element for each letter
+    let speed = 1;
+    for (let i = 0; i < txt.length; i++) {
+        let letter = document.createElement("span");
+        letter.speed = speed;
+        letter.innerText = txt[i];
+        letter.classList.add("nameLetter");
+        parent.appendChild(letter)
+        letter.style.opacity = 1;
+
+        // The speed increase of each letter - higher number = letters rise faster
+        speed = speed * 1.3;
+    }
+
+    let letters = document.querySelectorAll(`.${className} > *`);
+    window.addEventListener("scroll", function () {
+        for (let i = 0; i < txt.length; i++) {
+            letters[i].style.transform = `translate(0, -${window.scrollY * letters[i].speed}px)`;
+            letters[i].style.opacity = `${1 - (window.scrollY / 900)}`;
+            letters[i].style.filter = `blur(${window.scrollY / 500}px)`;
+        }
+    });
+}
+
+
+window.addEventListener("load", function () {
+    // Call functions to modify both containers
+    splitLetters("nameLetterContainer1");
+    splitLetters("nameLetterContainer2");
+
+    // Get all elements with tag "hidden" and observe them
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));
+});
+
